80fe60bd8c77d981f205d84b8e551f1745e99b81
hamzabida <dakibida156@gmail.com>
2026-05-13 10:37:50 -0400

    🎨 fix: simplify hero animation to ensure visibility

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,8 @@ import {useGSAP} from '@gsap/react'
 
 
 
-
 export default function Hero() {
 
-  const container = useRef(null)
   const text = useRef(null)
   const paragraph = useRef(null)
   const button1 = useRef(null)
@@ -18,7 +16,6 @@ export default function Hero() {
   useGSAP(() => {
     const tl=gsap.timeline()
 
-     tl.fromTo(container.current, {opacity:0},{opacity:1,duration:1.5}, 0)
      tl.fromTo(text.current, {y:50 , opacity:0},{y:0, opacity:1,duration:1}, 0.5)
      tl.fromTo(paragraph.current, {y:50 , opacity:0},{y:0, opacity:1,duration:1}, 0.7)
      tl.fromTo(button1.current, { opacity:0},{y:0, opacity:1,duration:0.8}, 0.9)
@@ -29,7 +26,7 @@ export default function Hero() {
 
   
   return (
-    <section ref={container} className="min-h-screen flex items-center justify-center bg-amber-50 text-black">
+    <section className="min-h-screen flex items-center justify-center bg-amber-50 text-black">
       <div className="text-center px-4">
         <h1 ref={text} className="text-5xl md:text-black lg:text-amber-400 font-bold mb-6 bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-transparent">
           Welcome to Our Site
